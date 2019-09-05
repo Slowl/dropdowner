90e4bff8f492bec2537a53689c82985fddf8f451
Slowl <hedi0475@gmail.com>
2019-09-05 19:40:21 +0200

    added bad final dropdown

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Hovered from './components/Hovered'
 import Title from './components/Title'
 import Listing from './components/Listing'
 import SearchCpt from './components/SearchCpt'
+import Final from './components/Final'
 
 const NavContainer = styled.nav`
   background-color: #1d1d1d;
+  display: flex;
+  justify-content: center;
 `
 const NavLink = styled(Link)`
   display: inline-block;
@@ -20,31 +23,70 @@ const NavLink = styled(Link)`
 `
 
 const RouterContainer = styled(Router)`
-  padding-top: 3em;
+  padding-top: em;
   display: flex;
   justify-content: center;
 `
 
-const App = () => {
-  return(
-    <div>
-      <NavContainer>
-        <NavLink to="/"> Default </NavLink>
-        <NavLink to="/hovered"> Hovered </NavLink>
-        <NavLink to="/title"> Title </NavLink>
-        <NavLink to="/listing"> Listing </NavLink>
-        <NavLink to="/search"> Search </NavLink>
-      </NavContainer>
+class App extends React.Component {
 
-      <RouterContainer>
-        <Default path="/" />
-        <Hovered path="/hovered" />
-        <Title path="/title" />
-        <Listing path="/listing" />
-        <SearchCpt path="/search" />
-      </RouterContainer>
-    </div>
-  )
+  state = {
+    data: '',
+  }
+
+  componentDidMount(){
+    fetch("https://cors-anywhere.herokuapp.com/https://api.ulule.com/v1/search/projects?lang=fr")
+      .then(res => {
+        return res.json()
+      }
+    ).then(data => {
+      this.setState({ data: data.projects })
+      console.log(this.state.data)
+    })
+  }
+
+  render(){
+    const dataOld = [
+      {id:1, title: "Option 1"},
+      {id:2, title: "Option 2"},
+      {id:3, title: "Option 3"},
+      {id:4, title: "Option 4"},
+      {id:5, title: "Option 5"},
+      {id:6, title: "Option 6"},
+      {id:7, title: "Option 7"},
+      {id:8, title: "Option 8"},
+      {id:9, title: "Option 9"},
+      {id:10, title: "Option 10"},
+      {id:11, title: "Option 11"},
+      {id:12, title: "Option 12"},
+      {id:13, title: "Option 13"},
+      {id:14, title: "Option 14"},
+      {id:15, title: "Option 15"},
+      {id:16, title: "Option 16"},
+    ]
+
+    return (
+      <div>
+        <NavContainer>
+          <NavLink to="/"> Default </NavLink>
+          <NavLink to="/hovered"> Hovered </NavLink>
+          <NavLink to="/title"> Title </NavLink>
+          <NavLink to="/listing"> Listing </NavLink>
+          <NavLink to="/search"> Search </NavLink>
+          <NavLink to="/final"> Final </NavLink>
+        </NavContainer>
+
+        <RouterContainer>
+          <Default path="/" />
+          <Hovered path="/hovered" />
+          <Title path="/title" />
+          <Listing path="/listing" data={dataOld} />
+          <SearchCpt path="/search" data={dataOld} />
+          <Final path="/final" data={this.state.data}/>
+        </RouterContainer>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/elements/Cards.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import styled from 'styled-components'
+import Selected from '../icons/Selected'
+
+import EggEmpty from '../icons/EggEmpty'
+import EggLow from '../icons/EggLow'
+import EggMid from '../icons/EggMid'
+import EggFull from '../icons/EggFull'
+import EggComplete from '../icons/EggComplete'
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 95%;
+  min-width: 95%;
+`
+
+const Placeholder = styled.span`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+`
+
+const Card = styled.div`
+  display: flex;
+  padding: .2em 0;
+  width: 100%;
+  min-width: 100%;
+`
+
+const ImgContainer = styled.div`
+  display: flex;
+  padding-right: .3em;
+
+  .selected-container {
+    padding: 1em .4em 1em 0;
+  }
+
+  img {
+    width: 65px;
+    height: 40px;
+    padding-top: .4em;
+  }
+`
+
+const InformationContainer = styled.div`
+  padding-left: .3em;
+  .title {
+    font-size: 14px;
+    color: #00A6F0;
+  }
+
+  .selected {
+    color: #00A6F0;
+  }
+
+  .desc {
+    color: #111111;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 20px;
+  }
+
+  .goal {
+    display: flex;
+    color: #BDBDBD;
+    padding: .4em 0 .2em 0;
+    font-size: 11px;
+    font-weight: 500;
+    line-height: 17px;
+
+    svg {
+      padding-right: 5px;
+    }
+  }
+`
+
+const Cards = ({ id, title, selectedId, selectedName, image, description, goal, reached, nb_product_sold, currency, type }) => {
+
+  const percentPriceReached = Math.round((reached / goal) * 100)
+  const percentSoldReached = Math.round((nb_product_sold / goal) * 100)
+
+  const CorrectEgg = (percent) => {
+    if (percent <= 15) {
+      return <EggEmpty width="14px" height="16px" />
+    } else if (percent >= 15 && percent <= 45) {
+      return <EggLow width="14px" height="16px" />
+    } else if (percent >= 45 && percent <= 75) {
+      return <EggMid width="14px" height="16px" />
+    } else if (percent >= 75 && percent < 100) {
+      return <EggFull width="14px" height="16px" />
+    } else if (percent >= 100) {
+      return <EggComplete width="14px" height="16px" />
+    } else {
+      return
+    }
+  }
+
+  return (
+    <CardsContainer>
+      <Card>
+        <ImgContainer>
+          <span className="selected-container">{selectedId.includes(id) ? <Selected width="10px" height="10px"/> : <Placeholder />}</span>
+          <img src={image} alt='project banner'/>
+        </ImgContainer>
+        <InformationContainer>
+          <span className="title"> {title} </span>
+          <div className="desc"> {description} </div>
+          <div className="goal">
+            {type === 1 ? CorrectEgg(percentSoldReached) : CorrectEgg(percentPriceReached)}
+            {type === 1 ? `${nb_product_sold} / ${goal}` : goal}
+            {type === 2 && currency}
+          </div>
+        </InformationContainer>
+
+
+      </Card>
+    </CardsContainer>
+  )
+}
+
+export default Cards
